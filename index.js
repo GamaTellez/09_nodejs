d8f53f6e9bd6b29eeaca17198d4610cd01d95f7b
Gama Tellez <gamatellez@gmail.com>
2019-03-10 09:39:18 -0600

    setting project up for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 var math = require('./functions');
 var app = express();
 
@@ -8,11 +8,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/main_view', function(request, response) {
+app.get('/', function(request, response) {
     response.render('main_view');
 });
 //listen to form from .ejs template with action = "/calc_function"
 app.get('/calc_function', math.shipping_math);
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
+//app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+if (PORT == null || PORT == "") {
+  PORT = 5000;
+}
+app.listen(PORT);
